08b2e7103ddefd3e5906e6f62e98ff080e16d961
Gordin <g0rdin@yahoo.de>
2009-08-20 17:53:50 +0200

    Added basic Graphic initialisation

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,60 @@
 #include <nds.h>
 
-int main() {
+void initVideo() {
+    /*
+     * Map VRAM to display a background on the main and sub screens.
+     *
+     * The vramSetMainBanks function takes four arguments, one for each of the
+     * major VRAM banks. We can use it as shorthand for assigning values to
+     * each of the VRAM bank's control registers.
+     * 
+     * We map  banks A and B to main screen background memory. This gives us
+     * 256KB, which is a healty amount for 16-bit graphics.
+     *
+     * We map bank C to sub screen background memory. 
+     *
+     * We map bank D to LCD. This setting is generally used for when we aren't
+     * using a particular bank.
+     *
+     * We map bank E to main screen sprite memory (aka object memory).
+     */
+    vramSetMainBanks(VRAM_A_MAIN_BG_0x06000000,
+                     VRAM_B_MAIN_BG_0x06020000,
+                     VRAM_C_SUB_BG_0x06200000,
+                     VRAM_D_LCD);
 
-    /* This program doesn't do very much, yet. */
+    vramSetBankE(VRAM_E_MAIN_SPRITE);
+
+    /* Set the video mode on the main screen. */
+    videoSetMode(MODE_5_2D | // Set the graphics mode to Mode 5
+                 DISPLAY_BG1_ACTIVE | // Enable BG3 for display
+                 DISPLAY_SPR_ACTIVE | // Enable sprites for display
+                 DISPLAY_SPR_1D );    // Enable 1D tiled sprites
+
+    /* Set the video mode on the sub screen. */
+    videoSetModeSub(MODE_5_2D | // Set the graphics mode to Mode 5
+                    DISPLAY_BG3_ACTIVE); // Enable BG3 for display
+}
+
+void initBackgrounds() {
+
+}
+
+void updateInput(touchPosition * touch) {
+    // Update the key registerts with current values.
+    scanKeys();
+    // Update the touch scren values.
+    touchRead(touch);
+}
+
+int main() {
+    /* Turn on the 2D graphics core. */
+    powerOn(POWER_ALL_2D);
+
+    /* Configure the VRAM and background control registers. */
+    lcdMainOnBottom(); // Place the main screen on the bottom physical screen
+    initVideo(); // Arrange VRAM Banks
+    initBackgrounds(); // Configure the background control register
 
 	return 0;
 }
